4f841ee15d8869af3bd3ed7bde0d33d7c1b0b1e1
AnFeliz <angel.esteban.feliz@gmail.com>
2023-09-23 08:15:56 -0400

    Improving importing process

--- 00-data/01-setting-project-up.R
+++ 00-data/01-setting-project-up.R
@@ -7,42 +7,42 @@ renv::restore()
 SourcePage <-
   rvest::read_html("https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page")
 
-ParquetLinks <-
-  rvest::html_elements(SourcePage,
-                       xpath = '//div[@class="faq-answers"]//li/a') |>
+TripLinks <-
+  SourcePage |>
+  rvest::html_elements(xpath = '//div[@class="faq-answers"]//li/a') |>
   rvest::html_attr("href") |>
-  grep(pattern = "fhvhv_[a-z]+_2023-\\d{2}\\.parquet", value = TRUE) |>
+  grep(pattern = "fhvhv_[a-z]+_2023-0[1-6]\\.parquet", value = TRUE) |>
   trimws()
 
-# 3. Creating a folder to store the data ----
-dir.create("00-data")
+TaxiZoneLink <-
+  SourcePage |>
+  rvest::html_elements(xpath = '//ul/li/a[text()="Taxi Zone Lookup Table"]')  |>
+  rvest::html_attr("href") |>
+  trimws()
 
-# 4. Downloading each file ----
-
-## Defining the path to save files
-LocalPath <- file.path("00-data", basename(ParquetLinks))
+# 3. Downloading each file ----
 
 ## This will make sure that R won't stop before
 ## downloading each parquet file
 options(timeout = 800)
 
-for(link_i in seq_along(ParquetLinks)){
-  download.file(ParquetLinks[link_i],
-                destfile = LocalPath[link_i],
+## Defining the path to save files
+TripLocalPath <- file.path("00-data", basename(TripLinks))
+
+## Saving Trip Parquet files
+for(link_i in seq_along(TripLinks)){
+  download.file(TripLinks[link_i],
+                destfile = TripLocalPath[link_i],
                 mode = "wb")
 }
 
-
-# 5. Downloading zone file ----
-SourcePage |>
-  rvest::html_elements(xpath = '//ul/li/a[text()="Taxi Zone Lookup Table"]')  |>
-  rvest::html_attr("href") |>
-  trimws() |>
-  download.file(destfile = "00-data/taxi_zone_lookup.csv",
-                mode = "wb")
+## Saving Taxi Zone CSV
+download.file(TaxiZoneLink,
+              destfile = file.path("00-data","taxi_zone_lookup.csv"),
+              mode = "wb")
 
 
-# 6. Splitting training and test data
+# 4. Splitting training and testing data ----
 
 ## As we a lot of data we can use 3 moths for training
 ## the rest of the months for testing the results
